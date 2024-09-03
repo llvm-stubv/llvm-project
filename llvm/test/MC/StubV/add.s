176021c8cffdb802b4adb3af320b6dce04948810
Fraser Cormack <fraser@codeplay.com>
2024-09-03 15:43:14 +0100

    [StubV] Add extra tests for MCInst dumping

--- llvm/test/MC/StubV/add.s
+++ llvm/test/MC/StubV/add.s
@@ -1,8 +1,21 @@
-# RUN: llvm-mc --triple stubv32 < %s -show-encoding | FileCheck %s
+# RUN: llvm-mc --triple stubv32 < %s -show-inst | FileCheck %s --check-prefix CHECK-INST
+# RUN: llvm-mc --triple stubv32 < %s -show-encoding | FileCheck %s --check-prefix CHECK-ENCODING
 
-# CHECK: encoding: [0x33,0x85,0xc5,0x00]
+# CHECK-ENCODING: encoding: [0x33,0x85,0xc5,0x00]
+# CHECK-INST:      <MCInst #295 ADD
+# CHECK-INST-NEXT:  <MCOperand Reg:11>
+# CHECK-INST-NEXT:  <MCOperand Reg:12>
+# CHECK-INST-NEXT:  <MCOperand Reg:13>>
 add a0, a1, a2
-# CHECK: encoding: [0x67,0x80,0x00,0x00]
+# CHECK-ENCODING: encoding: [0x67,0x80,0x00,0x00]
+# CHECK-INST:      <MCInst #296 JALR
+# CHECK-INST-NEXT:  <MCOperand Reg:1>
+# CHECK-INST-NEXT:  <MCOperand Reg:2>
+# CHECK-INST-NEXT:  <MCOperand Imm:0>>
 ret
-# CHECK: encoding: [0x67,0x80,0x00,0x00]
+# CHECK-ENCODING: encoding: [0x67,0x80,0x00,0x00]
+# CHECK-INST:      <MCInst #296 JALR
+# CHECK-INST-NEXT:  <MCOperand Reg:1>
+# CHECK-INST-NEXT:  <MCOperand Reg:2>
+# CHECK-INST-NEXT:  <MCOperand Imm:0>>
 jalr x0, 0(x1)
