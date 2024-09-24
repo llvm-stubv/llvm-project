ec30b4dc195b0e5b1b54fa0c66f58f633560a8f5
Alastair Murray <alastairmurray42@gmail.com>
2024-09-25 00:10:30 +0100

    Add support for SRA instruction

---
+++ llvm/test/MC/StubV/sra.s
@@ -0,0 +1,9 @@
+# RUN: llvm-mc --triple stubv32 < %s -show-inst | FileCheck %s --check-prefix CHECK-INST
+# RUN: llvm-mc --triple stubv32 < %s -show-encoding | FileCheck %s --check-prefix CHECK-ENCODING
+
+# CHECK-ENCODING: encoding: [0x33,0xd5,0xc5,0x40]
+# CHECK-INST:      <MCInst #297 SRA
+# CHECK-INST-NEXT:  <MCOperand Reg:11>
+# CHECK-INST-NEXT:  <MCOperand Reg:12>
+# CHECK-INST-NEXT:  <MCOperand Reg:13>>
+sra a0, a1, a2
