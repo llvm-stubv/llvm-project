1b8fc264718aaba8c125501c668f242c7df13558
Fraser Cormack <fraser@codeplay.com>
2024-10-02 14:08:05 +0100

    [StubV] Scrub regexes for MCInst opcodes
    These fluctuate too often and aren't important to test.

--- llvm/test/MC/StubV/add.s
+++ llvm/test/MC/StubV/add.s
@@ -2,19 +2,19 @@
 # RUN: llvm-mc --triple stubv32 < %s -show-encoding | FileCheck %s --check-prefix CHECK-ENCODING
 
 # CHECK-ENCODING: encoding: [0x33,0x85,0xc5,0x00]
-# CHECK-INST:      <MCInst #295 ADD
+# CHECK-INST:      <MCInst #{{[0-9]+}} ADD
 # CHECK-INST-NEXT:  <MCOperand Reg:11>
 # CHECK-INST-NEXT:  <MCOperand Reg:12>
 # CHECK-INST-NEXT:  <MCOperand Reg:13>>
 add a0, a1, a2
 # CHECK-ENCODING: encoding: [0x67,0x80,0x00,0x00]
-# CHECK-INST:      <MCInst #296 JALR
+# CHECK-INST:      <MCInst #{{[0-9]+}} JALR
 # CHECK-INST-NEXT:  <MCOperand Reg:1>
 # CHECK-INST-NEXT:  <MCOperand Reg:2>
 # CHECK-INST-NEXT:  <MCOperand Imm:0>>
 ret
 # CHECK-ENCODING: encoding: [0x67,0x80,0x00,0x00]
-# CHECK-INST:      <MCInst #296 JALR
+# CHECK-INST:      <MCInst #{{[0-9]+}} JALR
 # CHECK-INST-NEXT:  <MCOperand Reg:1>
 # CHECK-INST-NEXT:  <MCOperand Reg:2>
 # CHECK-INST-NEXT:  <MCOperand Imm:0>>
