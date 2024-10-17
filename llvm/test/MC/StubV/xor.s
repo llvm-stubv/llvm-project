01c21a3462298469255adf74e45619414a2eb8d9
Jefferson Le Quellec <jefferson.lequellec@codeplay.com>
2024-10-17 18:11:48 +0200

    Add tests for XORI instruction

--- llvm/test/MC/StubV/xor.s
+++ llvm/test/MC/StubV/xor.s
@@ -7,3 +7,24 @@
 # CHECK-INST-NEXT:  <MCOperand Reg:12>
 # CHECK-INST-NEXT:  <MCOperand Reg:13>>
 xor a0, a1, a2
+
+# CHECK-ENCODING: encoding: [0x13,0xc5,0xc5,0x00]
+# CHECK-INST: <MCInst #{{.*}} XORI
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Imm:12>>
+xor a0, a1, 12
+
+# CHECK-ENCONDING: encoding: [0x13,0xc5,0xc5,0x00]
+# CHECK-INST: <MCInst #308 XORI
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Imm:12>>
+xori a0, a1, 12
+
+# CHECK-ENCODING: encoding: [0x13,0xc5,0xf5,0xff]
+# CHECK-INST: <MCInst #308 XORI
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Imm:-1>>
+not a0, a1
