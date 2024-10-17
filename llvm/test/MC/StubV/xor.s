f47f392ed805bf3e65c5441df50d6cb2146edb5f
Jefferson Le Quellec <jefferson.lequellec@codeplay.com>
2024-10-17 18:11:56 +0200

    Apply code formatting

--- llvm/test/MC/StubV/xor.s
+++ llvm/test/MC/StubV/xor.s
@@ -2,10 +2,10 @@
 # RUN: llvm-mc --triple stubv32 < %s -show-encoding | FileCheck %s --check-prefix CHECK-ENCODING
 
 # CHECK-ENCODING: encoding: [0x33,0xc5,0x65,0x00]
-# CHECK-INST:      <MCInst #{{.*}} XOR
-# CHECK-INST-NEXT:  <MCOperand Reg:11>
-# CHECK-INST-NEXT:  <MCOperand Reg:12>
-# CHECK-INST-NEXT:  <MCOperand Reg:13>>
+# CHECK-INST: <MCInst #{{.*}} XOR
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Reg:13>>
 xor a0, a1, a2
 
 # CHECK-ENCODING: encoding: [0x13,0xc5,0xc5,0x00]
