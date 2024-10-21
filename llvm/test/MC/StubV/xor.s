07275119c58eb813d55e7e0092709ac344a224c2
Jefferson Le Quellec <jefferson.lequellec@codeplay.com>
2024-10-21 16:49:25 +0200

    Add support for XORI instruction (#12)
    * The definition of the XORI instruction
    * The patterns for the XOR and XORI instructions
    * The instruction alias for the special XORI special case
    * Its relevant tests

--- llvm/test/MC/StubV/xor.s
+++ llvm/test/MC/StubV/xor.s
@@ -2,8 +2,29 @@
 # RUN: llvm-mc --triple stubv32 < %s -show-encoding | FileCheck %s --check-prefix CHECK-ENCODING
 
 # CHECK-ENCODING: encoding: [0x33,0xc5,0x65,0x00]
-# CHECK-INST:      <MCInst #{{.*}} XOR
-# CHECK-INST-NEXT:  <MCOperand Reg:11>
-# CHECK-INST-NEXT:  <MCOperand Reg:12>
-# CHECK-INST-NEXT:  <MCOperand Reg:13>>
+# CHECK-INST: <MCInst #{{.*}} XOR
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Reg:13>>
 xor a0, a1, a2
+
+# CHECK-ENCODING: encoding: [0x13,0xc5,0xc5,0x00]
+# CHECK-INST: <MCInst #{{.*}} XORI
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Imm:12>>
+xor a0, a1, 12
+
+# CHECK-ENCONDING: encoding: [0x13,0xc5,0xc5,0x00]
+# CHECK-INST: <MCInst #{{.*}} XORI
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Imm:12>>
+xori a0, a1, 12
+
+# CHECK-ENCODING: encoding: [0x13,0xc5,0xf5,0xff]
+# CHECK-INST: <MCInst #{{.*}} XORI
+# CHECK-INST-NEXT: <MCOperand Reg:11>
+# CHECK-INST-NEXT: <MCOperand Reg:12>
+# CHECK-INST-NEXT: <MCOperand Imm:-1>>
+not a0, a1
