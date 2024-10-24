2aa4b774e5edc07206c9d61fb337f15f2d52a2b6
Fraser Cormack <fraser@codeplay.com>
2024-10-24 16:42:16 +0100

    [StubV] Provide instruction class hierarchies to simplify TableGen
    These hierarchies are just copied from RISC-V.
    
    Fixed a bug in the XOR encoding which didn't align with the RISC-V spec.

--- llvm/test/MC/StubV/xor.s
+++ llvm/test/MC/StubV/xor.s
@@ -1,7 +1,7 @@
 # RUN: llvm-mc --triple stubv32 < %s -show-inst | FileCheck %s --check-prefix CHECK-INST
 # RUN: llvm-mc --triple stubv32 < %s -show-encoding | FileCheck %s --check-prefix CHECK-ENCODING
 
-# CHECK-ENCODING: encoding: [0x33,0xc5,0x65,0x00]
+# CHECK-ENCODING: encoding: [0x33,0xc5,0xc5,0x00]
 # CHECK-INST: <MCInst #{{.*}} XOR
 # CHECK-INST-NEXT: <MCOperand Reg:11>
 # CHECK-INST-NEXT: <MCOperand Reg:12>
